eb608698b1409c315e655aa5f8f1d723d3153488
DerakhshanRadbareh <146732331+DerakhshanRadbareh@users.noreply.github.com>
2025-01-13 20:16:02 +0100

    Add SP for inserting into pfofiles

--- sql/stored_procedures.sql
+++ sql/stored_procedures.sql
@@ -14,3 +14,23 @@ $BODY$;
 ALTER PROCEDURE public.sp_insert_into_invitations(text, integer)
     OWNER TO postgres;
 
+
+CREATE OR REPLACE PROCEDURE public.sp_insert_into_profiles(
+	IN _profile_photo_link text,
+	IN _age integer,
+	IN _language text,
+	IN _name text,
+	IN _userid integer,
+	IN _family text)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+    -- Insert the new profile into the table and return the inserted data
+    INSERT INTO public."Profiles" 
+    (profile_photo_link, age, language, name, userid, family)
+    VALUES
+    (_profile_photo_link, _age, _language, _name, _userid, _family);
+END;
+$BODY$;
+ALTER PROCEDURE public.sp_insert_into_profiles(text, integer, text, text, integer, text)
+    OWNER TO postgres;
