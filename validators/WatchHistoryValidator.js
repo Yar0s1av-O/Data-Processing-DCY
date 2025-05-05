18ff4bf85846a7107c7450a322d5cfdd6a6d7af9
Yar0s1av-O <yaroslav.oleinychenko@student.nhlstenden.com>
2025-05-05 22:00:25 +0200

    validator fix

--- validators/WatchHistoryValidator.js
+++ validators/WatchHistoryValidator.js
@@ -4,14 +4,14 @@ const validateWatchHistoryCreate = (data) => {
     const schema = Joi.object({
         profile_id: Joi.number().integer().required(),
         watchable_id: Joi.number().integer().required(),
-        time_stopped: Joi.number().min(0).required(),
+        time_stopped: Joi.string().pattern(/^\d{2}:\d{2}:\d{2}$/).required(), // HH:MM:SS format
     });
     return schema.validate(data);
 };
 
 const validateWatchHistoryUpdate = (data) => {
     const schema = Joi.object({
-        time_stopped: Joi.number().min(0).required(),
+        time_stopped: Joi.string().pattern(/^\d{2}:\d{2}:\d{2}$/).required(), // HH:MM:SS format
     });
     return schema.validate(data);
 };
